a45051ccb2d43f1964222577a9d07d548224e2d7
Jamie Hall <the.sly.marbo@googlemail.com>
2013-06-20 11:45:14 +0100

    Structural improvements

--- doc.go
+++ doc.go
@@ -11,32 +11,32 @@ Example usage:
 
 		func main() {
 			scanner := command.NewScanner(os.Stdin, true) // Read from stdin.
-	
+
 			for scanner.Scan() {
 				t := scanner.Token()
 				switch {
-			
+
 					// Check for an empty token.
 					case token.Empty():
 						// ...
-			
+
 					// Compare against one string.
 					case token.Equals("example"):
 						// ...
-				
+
 					// Compare against multiple strings.
 					case token.Equals("more", "other"):
 						// ...
-				
+
 					// Check for a prefix.
 					case token.HasPrefix("start"):
 						token.String() // Full string, starting with "start".
 						token.Body()   // Modified string, starting after "start".
-				
+
 					// HasSuffix works identically.
 				}
 			}
-	
+
 			if err := scanner.Err(); err != nil {
 				// handle error.
 			}
